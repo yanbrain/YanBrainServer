a30a796c885fc96e9cfc1da84227f2a74dccba10
yanbrain <artem.aslanyan@gmail.com>
2026-01-09 11:55:57 +0400

    Fix dropdown positioning in admin dashboard

--- packages/admindash/src/components/ui/dropdown-menu.tsx
+++ packages/admindash/src/components/ui/dropdown-menu.tsx
@@ -35,9 +35,13 @@ DropdownMenuSubTrigger.displayName =
 const DropdownMenuSubContent = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.SubContent>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.SubContent>
->(({ className, ...props }, ref) => (
+>(({ className, sideOffset = 4, ...props }, ref) => (
   <DropdownMenuPrimitive.SubContent
     ref={ref}
+    side="left"
+    align="start"
+    sideOffset={sideOffset}
+    collisionPadding={8}
     className={cn(
       'glass-panel z-50 min-w-[8rem] overflow-hidden border border-white/10 bg-transparent p-1 text-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
       className
@@ -55,7 +59,10 @@ const DropdownMenuContent = React.forwardRef<
   <DropdownMenuPrimitive.Portal>
     <DropdownMenuPrimitive.Content
       ref={ref}
+      side="left"
+      align="start"
       sideOffset={sideOffset}
+      collisionPadding={8}
       className={cn(
         'glass-panel z-50 min-w-[8rem] overflow-hidden border border-white/10 bg-transparent p-1 text-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
         className
